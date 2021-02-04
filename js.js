ec4556d4d988c1c2367614a3f2092e439fca48d2
OlhaNester <olga_nv@ukr.net>
2021-02-04 11:44:25 +0200

    исправления

--- js.js
+++ js.js
@@ -2,45 +2,46 @@ class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
+    this.refs = {
+      daysValue: document.querySelector('span[data-value="days"]'),
+      hoursValue: document.querySelector('span[data-value="hours"]'),
+      minsValue: document.querySelector('span[data-value="mins"]'),
+      secsValue: document.querySelector('span[data-value="secs"]'),
+    };
   }
   start() {
     this.idTimer = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = this.targetDate.getTime() - currentTime;
 
-      updateTimer(deltaTime);
+      this.updateTimer(deltaTime);
       if (deltaTime <= 0) {
         clearInterval(this.idTimer);
       }
     }, 1000);
   }
-}
 
-const refs = {
-  daysValue: document.querySelector('span[data-value="days"]'),
-  hoursValue: document.querySelector('span[data-value="hours"]'),
-  minsValue: document.querySelector('span[data-value="mins"]'),
-  secsValue: document.querySelector('span[data-value="secs"]'),
-};
-
-const updateTimer = function (time) {
-  if (time < 0) {
-    time = 0;
+  updateTimer = function (time) {
+    if (time < 0) {
+      time = 0;
+    }
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    this.valueTimer(days, hours, mins, secs);
+  };
+  valueTimer(days, hours, mins, secs) {
+    this.refs.daysValue.textContent = `${days}`;
+    this.refs.hoursValue.textContent = `${hours}`;
+    this.refs.minsValue.textContent = `${mins}`;
+    this.refs.secsValue.textContent = `${secs}`;
   }
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  refs.daysValue.textContent = `${days}`;
-  refs.hoursValue.textContent = `${hours}`;
-  refs.minsValue.textContent = `${mins}`;
-  refs.secsValue.textContent = `${secs}`;
-};
-
-function pad(value) {
-  return String(value).padStart(2, "0");
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
 }
 
 const timer = new CountdownTimer({
